b9c110706b158f3d089c5d606a17f04fbf1b21af
Jack Aardal <jaardal@glneurotech.com>
2024-03-13 14:57:39 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CommunityToastBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace CommunityToastBug
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            try
+            {
+                var toast = Toast.Make("Yeah toast!");
+                await toast.Show();
+            }
+            catch (Exception ex)
+            {
+                errorLabel.Text = ex.ToString();
+            }
+        }
+    }
+
+}
